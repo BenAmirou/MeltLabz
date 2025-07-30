944f04f571471efc39300a91345cfd2d19a0c32f
BenAmirou <114466448+BenAmirou@users.noreply.github.com>
2025-07-31 04:05:35 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,14 @@ from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, WebAppI
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = "8058105739:AAGYQ2goMQqS1KOTaHQ9e6zTIfcTDJv1MiA"
-WEBAPP_URL = "https://meltlabz.us/"  # ← à adapter
+WEBAPP_URL = "https://meltlabz.us/"  # ← Remplace par ton vrai lien
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [[
         InlineKeyboardButton("🧪 Voir les Produits", web_app=WebAppInfo(url=WEBAPP_URL))
     ]]
     await update.message.reply_text(
-        "👋 *Bienvenue sur MeltLabz !*\nClique sur le bouton ci-dessous pour voir les produits 🔬",
+        "👋 *Bienvenue sur MeltLabz !*\\nClique sur le bouton ci-dessous pour voir les produits 🔬",
         parse_mode="Markdown",
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
